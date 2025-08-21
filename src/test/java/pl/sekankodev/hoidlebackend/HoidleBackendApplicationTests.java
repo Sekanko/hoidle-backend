59e73efa652bdd3d4ee37c76ea9cdeea5ade247a
Oskar Tarka <oskar0208@gmail.com>
2025-08-21 21:14:13 +0200

    inital commit
    I'm moving hoidle-backend from Gradle to Maven

---
+++ src/test/java/pl/sekankodev/hoidlebackend/HoidleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sekankodev.hoidlebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoidleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
